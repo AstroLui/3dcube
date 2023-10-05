ed1dabb77d249d741442a8e6a053b5cbe3ecf17d
JAfricanoT <josefranciscoat.correo@gmail.com>
2023-10-05 10:14:01 -0400

    feat: better rotation

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const cubo = document.querySelector(".cubo")
 
-function cambiarArita(param) {
-    cubo.style.transform = `rotate3d(${param})`
+function cambiarArista(paramCurr, paramNext) {
+    cubo.style.transform = `scale3d(0.75,0.75,0.75) rotate3d(${paramCurr})`
+    // cubo.style.transform = `rotate3d(${paramNext})`
+    setTimeout(() => {
+        cubo.style.transform = `scale3d(0.75,0.75,0.75) rotate3d(${paramNext})`
+      }, "1500");
+    
+      setTimeout(() => {
+        cubo.style.transform = `scale3d(1,1,1) rotate3d(${paramNext})`
+      }, "3000");
 }
